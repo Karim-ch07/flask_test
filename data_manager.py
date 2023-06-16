81fff624bb79af23017a2173e2fc5760e28f7482
Karim-ch07 <chihanikarim07@gmail.com>
2023-06-16 14:26:55 +0200

    -

--- data_manager.py
+++ data_manager.py
@@ -10,37 +10,21 @@ DB_NAME = "flightdb"
 DB_PORT = "3306"
 
 # Establish the database connection
-def establish_db_connection():
-    try:
-        # Attempt to establish the MySQL database connection
-        db = mysql.connector.connect(
-            host=DB_HOST,
-            user=DB_USER,
-            password=DB_PASSWORD,
-            database=DB_NAME,
-            port=DB_PORT
-        )
-
-        # If successful, return the connection object
-        return db
-
-    except mysql.connector.Error as e:
-        # Handle the error if connection establishment fails
-        handle_failure()
-        return None
-
-def handle_failure():
-    print("Failed to establish a connection to the database.")
-
+db = mysql.connector.connect(
+    host=DB_HOST,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    database=DB_NAME
+)
 class DataManager:
     def __init__(self):
         self.dest_info = []
         self.cx_info = []
-        self.db = establish_db_connection()
+        # self.db = establish_db_connection()
 
     def get_dest_info(self):
         # Retrieve data from the MySQL database
-        cursor = self.db.cursor()
+        cursor = db.cursor()
         cursor.execute("SELECT * FROM prices")
         self.dest_info = cursor.fetchall()
         cursor.close()
@@ -48,12 +32,12 @@ class DataManager:
 
     def update_dest_info_iata(self):
         # Update data in the MySQL database
-        cursor = self.db.cursor()
+        cursor = db.cursor()
         for dest in self.dest_info:
             sql = "UPDATE prices SET IATA = %s WHERE id = %s"
             val = (dest[2], dest[0])
             cursor.execute(sql, val)
-            self.db.commit()
+            db.commit()
         cursor.close()
 
     # def get_cx_info(self):
